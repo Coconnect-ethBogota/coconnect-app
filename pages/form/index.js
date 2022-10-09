86b6ef690fe206b14247c08f95537b1521610553
Matias Rodriguez <matias.demian.r@gmail.com>
2022-10-08 22:25:04 -0500

    Commit

--- pages/form/index.js
+++ pages/form/index.js
@@ -4,55 +4,112 @@ import {
     FormControl,
     FormLabel,
     Input,
+    HStack,
     Stack,
     Link,
     Button,
     Heading,
     Text,
     useColorModeValue,
+    Container,
+    Avatar,
+    Textarea,
   } from '@chakra-ui/react';
   
   export default function Form() {
     return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Collaborate </Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-            with your favorite <Link color={'blue.400'}>creator</Link> 🌿
-            </Text>
+      <Container maxW="sm">
+        <Stack ml="24px" mr="24px">
+          <Stack mt="34px">
+              <HStack h="52px" w="342px"
+                  borderRadius={'40px'}
+                  bgGradient="linear(to-r, #FA5985, #FDC731)"
+                  color={'#FFFFFF'}>
+                    <Stack></Stack>
+                    <Stack></Stack>
+              </HStack>
           </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel>Text</FormLabel>
-                <Input type="text" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>IPFS url</FormLabel>
-                <Input type="text" />
-              </FormControl>
-              <Stack spacing={10}>
-                <Button
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Contact 1 Matic
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
         </Stack>
-      </Flex>
+       <CompleteForm />
+      </Container>
     );
+  }
+
+
+  const CompleteForm = () => {
+    return (
+      <>
+        <Stack ml="24px" mr="24px">
+          <HStack justifyContent={'space-between'}>
+            <Avatar mt="28px" ml="27px" />
+            <HStack>
+              <Avatar mt="28px" ml="80px"/>
+                <Stack  h="29px" w="104px" justify={'center'} align={'end'}>
+                  <Text mt="28px" fontWeight={800} fontSize="14px" whiteSpace={'pre'}>1 MATIC</Text>
+                  <Text mt="28px" fontWeight={800} fontSize="14px" whiteSpace={'pre'}>Aprox per post</Text>
+                </Stack>
+            </HStack>
+          </HStack>
+          <Stack>
+            <HStack mt="28px">
+              <Input boxShadow='lg' h="50px" w="342px" placeholder="Url" borderRadius={'12px'}/>
+              <Input boxShadow='lg' h="50px" w="342px" placeholder="Url" borderRadius={'12px'}/>
+            </HStack>
+          </Stack>
+          <Stack>
+            <Input mt="28px" boxShadow='lg' h="50px" w="342px" placeholder="Url" borderRadius={'12px'}/>
+          </Stack>
+          <Stack>
+            <Text mt="20px" fontWeight={800} fontSize="14px" whiteSpace={'pre'}>Amount per second</Text>
+          </Stack>
+          <Stack>
+            <Textarea boxShadow='lg' mt="29px" h="220px" w="342px" placeholder="Text" borderRadius={'12px'}/>
+          </Stack>
+          <Stack>
+            <Input boxShadow='lg' mt="5px" h="50px" w="342px" placeholder="Url" borderRadius={'12px'}/>
+          </Stack>
+          <Stack>
+            <Button mt="20px" h="54px" w="343px" mb="34px"
+                  borderRadius={'12px'}
+                  bgGradient="linear(to-r, #FA5985, #FDC731)"
+                  color={'#FFFFFF'}
+                  > Send Proposal
+            </Button> 
+          </Stack>
+        </Stack>
+      </>
+    )
+  }
+
+  const SimpleForm = () => {
+    return (
+      <>
+        <Stack ml="24px" mr="24px">
+          <HStack justifyContent={'space-between'}>
+            <Avatar mt="28px" ml="27px" />
+            <HStack>
+              <Avatar mt="28px" ml="80px"/>
+                <Stack  h="29px" w="104px" justify={'center'} align={'end'}>
+                  <Text mt="28px" fontWeight={800} fontSize="14px" whiteSpace={'pre'}>1 MATIC</Text>
+                  <Text mt="28px" fontWeight={800} fontSize="14px" whiteSpace={'pre'}>Aprox per post</Text>
+                </Stack>
+            </HStack>
+          </HStack>
+          <Stack>
+            <Textarea boxShadow='lg' mt="29px" h="351px" w="342px" placeholder="Text" borderRadius={'12px'}/>
+          </Stack>
+          <Stack>
+            <Input boxShadow='lg' mt="5px" h="50px" w="342px" placeholder="Url" borderRadius={'12px'}/>
+          </Stack>
+          <Stack>
+            <Button mt="20px" h="54px" w="343px" mb="34px"
+                  borderRadius={'12px'}
+                  bgGradient="linear(to-r, #FA5985, #FDC731)"
+                  color={'#FFFFFF'}
+                  > Send Proposal
+            </Button> 
+          </Stack>
+        </Stack>
+      </>
+    )
   }
