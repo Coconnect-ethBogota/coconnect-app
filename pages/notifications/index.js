197e33cf66f1d055b378280d2fbb99b2b74cabf3
FedeL <FedeL>
2022-10-09 03:31:56 -0500

    style

--- pages/notifications/index.js
+++ pages/notifications/index.js
@@ -16,8 +16,14 @@ import {
 import { useState } from 'react'
 import NotificationIcon from '../../components/icons/NotificationIcon'
 import ProposalsIcon from '../../components/icons/ProposalsIcon'
+import * as PushAPI from "@pushprotocol/restapi";
+import * as ethers from "ethers";
 
-const data = [
+const PK = '410f0303402e5311f3a6f5d7859222ebe7c94e3e8b1afe981ede1e75a02e8bfb'; // channel private key
+const Pkey = `0x${PK}`;
+const signer = new ethers.Wallet(Pkey);
+
+const dataaa = [
   {
     status: 'pending',
     variant: 'gray',
@@ -26,22 +32,103 @@ const data = [
     id: 2,
   },
   {
-    status: 'denegada',
+    status: 'denied',
     variant: 'red',
     name: 'Juanma.lens',
     image: 'https://img.seadn.io/files/b0a2331a53def406ee438364ed530b4f.png?fit=max&w=1000',
     id: 1,
   },
   {
-    status: 'aceptada',
+    status: 'accepted',
     variant: 'green',
     name: 'Mati.lens',
     image: 'https://bit.ly/ryan-florence',
     id: 3,
   },
 ]
+const sendMessageAccepted = async () => {
+  try {
+    const apiResponse = await PushAPI.payloads.sendNotification({
+      signer,
+      type: 3, // target
+      identityType: 2, // direct payload
+      notification: {
+        title: `🥥 COCOPUSH`,
+        body: `Your proposal was accepted ✅`
+      },
+      payload: {
+        title: `🥥 COCOPUSH`,
+        body:  `Your proposal was accepted ✅`,
+        cta: '',
+        img: ''
+      },
+      recipients: 'eip155:5:0xA081e1dA16133bB4Ebc7Aab1A9B0588A48D15138', // recipient address
+      channel: 'eip155:5:0x4A5b210B97DDa05cD26ad45bCBF18d015BD3693D', // your channel address
+      env: 'staging'
+    });
+    
+    // apiResponse?.status === 204, if sent successfully!
+    console.log('API repsonse: ', apiResponse);
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+}
+
+const sendMessageDenied = async () => {
+  try {
+    const apiResponse = await PushAPI.payloads.sendNotification({
+      signer,
+      type: 3, // target
+      identityType: 2, // direct payload
+      notification: {
+        title: `🥥 COCOPUSH`,
+        body: `Your proposal was denied ❌`
+      },
+      payload: {
+        title: `🥥 COCOPUSH`,
+        body:  `Your proposal was denied ❌`,
+        cta: '',
+        img: ''
+      },
+      recipients: 'eip155:5:0xA081e1dA16133bB4Ebc7Aab1A9B0588A48D15138', // recipient address
+      channel: 'eip155:5:0x4A5b210B97DDa05cD26ad45bCBF18d015BD3693D', // your channel address
+      env: 'staging'
+    });
+    
+    // apiResponse?.status === 204, if sent successfully!
+    console.log('API repsonse: ', apiResponse);
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+}
+
 export default function Notifications () {
   const [status, setstatus] = useState('pending')
+
+  const [data, setdata] = useState([
+    {
+      status: 'pending',
+      variant: 'gray',
+      name: 'Hollyherdon.lens',
+      image: 'https://img.seadn.io/files/7a52159b156c616c0d632dc05c25dc1a.png?fit=max&w=1000',
+      id: 2,
+    },
+    {
+      status: 'denied',
+      variant: 'red',
+      name: 'Juanma.lens',
+      image: 'https://img.seadn.io/files/b0a2331a53def406ee438364ed530b4f.png?fit=max&w=1000',
+      id: 1,
+    },
+    {
+      status: 'accepted',
+      variant: 'green',
+      name: 'Mati.lens',
+      image: 'https://bit.ly/ryan-florence',
+      id: 3,
+    },
+  ])
+
   return (
     <>
       <HStack justify={'space-between'} pt={'25px'}>
@@ -54,7 +141,6 @@ export default function Notifications () {
       </HStack>
       <Stack alignItems={'center'} w={'100%'} mt={'20px'}>
       <SwipeUp status={status} onClick={setstatus}/>
-      <ProposalCard />
       {data.map((person) => {
         return <Card props={person} key={person.key} />
       })}
@@ -76,13 +162,6 @@ const SwipeUp = ({status, onClick}) => {
   )
 }
 
-const ProposalCard = () => {
-  return (
-    <HStack>
-
-    </HStack>
-  )
-}
 const Card = ({ props }) => {
   const [open, setopen] = useState(false)
   return (
@@ -110,8 +189,8 @@ const Card = ({ props }) => {
         <Stack>
           {props.status == 'pending' && (
             <HStack>
-              <IconButton w={'40px'} variant='ghost' isRound aria-label="Search database" icon={<CheckIcon color={'#FF5E51'}/>} border={'2px solid #FF5E51'} />
-              <IconButton w={'40px'} variant='ghost'  isRound aria-label="Search database" icon={<CloseIcon color={'#00D387'}  />} border={'2px solid #00D387'}/>
+              <IconButton onClick={sendMessageAccepted} w={'40px'} variant='ghost' isRound aria-label="Search database" icon={<CheckIcon color={'#00D387'} />} border={'2px solid #00D387'} />
+              <IconButton onClick={sendMessageDenied} w={'40px'} variant='ghost'  isRound aria-label="Search database" icon={<CloseIcon  color={'#FF5E51'} />} border={'2px solid #FF5E51'}/>
             </HStack>
           )}
         </Stack>
